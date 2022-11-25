d55a2fdc40c96b52f6486b2442897d478aff8c7b
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-25 14:47:39 +0100

    Inizializzata repo con esercizio come commento

--- csharp-geometria/Program.cs
+++ csharp-geometria/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿/*
+ * [] Creare una classe Rettangolo con due attributi interi: baseRettangolo e altezzaRettangolo.
+ *  Aggiungere due metodi: calcolaArea e calcolaPerimetro checalcolano e restituiscono, rispettivamente, l’area e il perimetro del rettangolo.
+ * 
+ * [] Poi, nel vostro programma principale (Program.cs) chiedete all’utente di inserire, da console,
+ *  i valori di base e di altezza con cui istanziare un nuovoRettangolo. Dopo averlo istanziato,
+ *  occupatevi di configurare le sue proprietà di base e altezza (come abbiamo fatto con le nostre auto questa mattina nelprogramma principale)
+ *  e stampate a video il perimetro e l’area.
+ * 
+ * [] Provate ad istanziare tanti rettangoli con nome diverso e attributi diversi
+ *  (baseRettangolo e altezzaRettangolo) e provate a stampare le loro proprietà adesempio con un metodo
+ *  StampaRettangolo che vi stampi il vostro rettangolo ad esempio cosi:
+ *      —— Rettangolo1 ——
+ *      base: 20 cm
+ *      altezza: 10 cm
+ *      Perimetro: 60 cm
+ *      Area: 200 cm2
+ */
